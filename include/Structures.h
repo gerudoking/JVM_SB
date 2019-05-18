6b0da8343178b5e1771c356043124dfb63c47d38
nalualvarez <nalualvarez@hotmail.com>
2019-05-18 16:27:16 -0300

    Update Structures.h
    classfile

--- include/Structures.h
+++ include/Structures.h
@@ -31,4 +31,27 @@ struct method_info{
 	attribute_info attributes[attributes_count];
 };
 typedef struct method_info method_info;
+
+struct ClassFile{
+  u4 magic;
+  u2 minor_version;
+  u2 major_version;
+  u2 constant_pool_count;
+  void 	**constant_pool;
+  u2 access_flags;
+  u2 this_class;
+  u2 super_class;
+  u2 interfaces_count;
+  u2 *interfaces;
+  u2 fields_count;
+  field_info *fields;
+  u2 methods_count;
+  method_info *methods;
+  u2 attributes_count;
+  void **attributes;
+};
+typedef struct ClassFile ClassFile;
+
+
+
 #endif
